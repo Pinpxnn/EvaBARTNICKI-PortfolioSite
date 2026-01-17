2abb151020239c96ce52a882e64c092e3329b1c1
Pinpxnn <eva3107b@gmail.com>
2026-01-17 14:24:30 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form")
+
+const nameInput = document.querySelector("#name")
+const nameError = document.querySelector("#name-error")
+
+const emailInput = document.querySelector("#email")
+const emailError = document.querySelector("#email-error")
+
+const messageInput = document.querySelector("#message")
+const messageError = document.querySelector("#message-error")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    if (nameInput.value === ""){
+        nameError.classList.remove("hidden")
+    }
+    else{
+        nameError.classList.add("hidden")
+        console.log("name:", nameInput.value);
+    }
+
+    if (emailInput.value === ""){
+        emailError.classList.remove("hidden")
+    }
+    else{
+        emailError.classList.add("hidden")
+        console.log("mail:", emailInput.value);
+    }
+
+    if (messageInput.value.trim().length < 40) {
+        messageError.classList.remove("hidden")
+        valid = false
+    } else {
+        messageError.classList.add("hidden")
+        console.log("message:", messageInput.value)
+    }
+})
